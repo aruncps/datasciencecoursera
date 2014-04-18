7dd014d85ea91a65ff19049f51b26aed2c3c3782
aruncps <aruncps@gmail.com>
2014-04-18 20:37:26 +0530

    message

---
+++ 1.R
@@ -0,0 +1,168 @@
+tute1 <- read.csv("C:/Mac Move/eBooks/Forecasting/tute1.csv")
+View(tute1)
+forecast <- read.csv("C:\Mac Move\eBooks\Forecasting\shinyapp _Forecast\Forecast.txt")
+forecast <- read.csv("C:/Mac Move/eBooks/Forecasting/shinyapp _Forecast/Forecast.txt")
+view(forecast)
+View(forecast)
+forecast <- read.csv("C:/Mac Move/eBooks/Forecasting/shinyapp _Forecast/Forecast.txt",sep = "\t")
+View(forecast)
+View(forecast)
+forecast <- read.csv("C:/Mac Move/eBooks/Forecasting/shinyapp _Forecast/Forecast.txt",sep = "\t")
+forecast <- read.csv("C:/Mac Move/eBooks/Forecasting/shinyapp/Forecast.txt",sep = "\t")
+View(forecast)
+plot(forecast)
+library("tseries", lib.loc="C:/Users/shanmuga/Documents/R/win-library/2.15")
+library("tools", lib.loc="C:/Program Files/R/R-2.15.2/library")
+library("tcltk", lib.loc="C:/Program Files/R/R-2.15.2/library")
+library("zoo", lib.loc="C:/Users/shanmuga/Documents/R/win-library/2.15")
+library("xtable", lib.loc="C:/Users/shanmuga/Documents/R/win-library/2.15")
+library("bitops", lib.loc="C:/Users/shanmuga/Documents/R/win-library/2.15")
+library("boot", lib.loc="C:/Program Files/R/R-2.15.2/library")
+detach("package:bitops", unload=TRUE)
+detach("package:boot", unload=TRUE)
+headerPanel("Salesforce"),
+# Sidebar with controls to select the variable to plot against mpg
+# and to specify whether outliers should be included
+sidebarPanel(
+selectInput("variable", "Variable:",
+tg
+forecast <- read.csv("C:/Mac Move/eBooks/Forecasting/shinyapp/Forecast.txt",sep = "\t")
+plot(melsyd[,"Economy.Class"],
+main="Economy class passengers: Melbourne-Sydney",
+xlab="Year",ylab="Thousands")
+view(a10)
+View(a10)
+library(fpp)
+View(a10)
+plot(a10)
+read.csv("C:/Mac Move/eBooks/Forecasting/shinyapp/Forecast.txt")
+read.csv("C:/Mac Move/eBooks/Forecasting/shinyapp/Forecast.txt",sep"\t")
+read.csv("C:/Mac Move/eBooks/Forecasting/shinyapp/Forecast.txt",sep="\t")
+forecast<-read.csv("C:/Mac Move/eBooks/Forecasting/shinyapp/Forecast.txt",sep="\t")
+View(forecast)
+plot(forecast$All)
+plot(forecast$All)
+fib <- function(n) ifelse(n<3, 1, fib(n-1)+fib(n-2))
+fib(5)
+fib(10)
+ts(forecast$All,frequency=4)
+plot(ts(forecast$All,frequency=4))
+plot(ts(forecast$All,frequency=52))
+seasonplot(ts(forecast$All,frequency=52))
+library(fpp)
+seasonplot(ts(forecast$All,frequency=52))
+X <- c(5,5.5,6,6,6,6.5,6.5,6.5,6.5,7,7,8,8,9)
+x
+X
+table(X)
+plot(table(X))
+freq <- table(X)
+feq
+freq
+data.frame(freq)
+sum(freq)
+freq/sum(freq)
+cumsum(freq/sum(freq))
+ex.1 <- read.csv("ex1.csv")
+ex.1 <- read.csv("C:\Mac Move\eBooks\R\ThinkStat\ex1.csv")
+ex.1 <- read.csv("C:\'Mac Move'\eBooks\R\ThinkStat\ex1.csv")
+ex.1 <- read.csv("C:\"Mac Move"\eBooks\R\ThinkStat\ex1.csv")
+ex.1 <- read.csv("C:/"Mac Move"/eBooks/R/ThinkStat/ex1.csv")
+ex.1 <- read.csv("C:/Mac Move/eBooks/R/ThinkStat/ex1.csv")
+hist(ex.1$height)
+boxplot(c(1,11.5,6,7.2,4,8,9,10,6.8,8.3,2,2,10,1))
+boxplot(c(1,11.5,6,7.2,4,8,9,10,6.8,8.3,2,2,10,19.5))
+boxplot(ex.1$height)
+hist(ex.1$height)
+hist(ex.1$height)
+boxplot(ex.1$height)
+summary(ex.1$height)
+library("forecast", lib.loc="C:/Users/shanmuga/Documents/R/win-library/2.15")
+library("fpp", lib.loc="C:/Users/shanmuga/Documents/R/win-library/2.15")
+library("rstudio", lib.loc="C:/Program Files/RStudio/R/library")
+library("shiny", lib.loc="C:/Users/shanmuga/Documents/R/win-library/2.15")
+x <- c(4,5,6,6,6,7,7,7,7,7,7,8,8,8,9,10)
+hist(x)
+x <- c(4,5,6,6,6,7,7,7,7,7,7,8)
+hist(x)
+x <- c(6,7,7,7,7,7,7,8,8,8,9,10)
+hist(x)
+Reponse.vs.Effort <- read.delim("~/CCAOperations/Dashboards/Reponse vs Effort.txt")
+View(Reponse.vs.Effort)
+response=Reponse.vs.Effort$xWE_Services_DA
+effort=Reponse.vs.Effort$effort
+plot(response)
+plot(effort)
+effort=Reponse.vs.Effort$Time_Taken
+plot(effort)
+effort<-Reponse.vs.Effort$Time_Taken
+plot(effort)
+Reponse.vs.Effort$Time_Taken
+View(Reponse.vs.Effort)
+View(Reponse.vs.Effort)
+View(Reponse.vs.Effort)
+View(Reponse.vs.Effort)
+Reponse.vs.Effort <- read.delim("~/CCAOperations/Dashboards/Reponse vs Effort.txt")
+View(Reponse.vs.Effort)
+response=Reponse.vs.Effort$xWE_Services_DA
+effort=Reponse.vs.Effort$Time_Taken
+plot(effort)
+Reponse.vs.Effort <- read.delim("~/CCAOperations/Dashboards/Reponse vs Effort.txt")
+View(Reponse.vs.Effort)
+response=Reponse.vs.Effort$xWE_Services_DA
+effort=Reponse.vs.Effort$Time_Taken
+effort
+effort=Reponse.vs.Effort$Time_taken
+plot(effort)
+x<-c(.5,.6)
+x
+m<-matrix(1:6,nrow=2,ncol=3)
+m
+dim(m)
+attributes(m)
+class(m)
+class(m[2])
+m<-1:10
+m
+dim(m)
+m
+dim(m)<-c(2,5)
+m
+dim(m)<-c(3,5)
+dim(m)<-c(5,2)
+m
+x<-c(1,3,5)
+y<-c(3,2,10)
+rbind(x,y)
+x<-1:4
+y<-2
+x+y
+x<-list(2,"a","b",TRUE)
+x
+x[[2]]
+class(x[[2]])
+x<-c(3,5,1,10,12,6)
+x
+x[x<=5]<-0
+x
+x<-4
+class(x)
+x<-4L
+class(x)
+x<-c(1,3,5)
+y<-c(3,2,10)
+cbind(x,y)
+x<-list(2,"a","b",TRUE)
+x
+x[[2]]
+x<- list(a=list(10,12,14),b=c(3.14,2.81))
+x[[c(1,3)]]
+x
+x<-c("a","b","C")
+as.numeric(x)
+as.logical(x)
+as.complex(x)
+getwd
+getwd()
+install.packages("swirl")
+savehistory("C:/Mac Move/eBooks/Coursera/Specialization/Data Science/Code/1.R")
